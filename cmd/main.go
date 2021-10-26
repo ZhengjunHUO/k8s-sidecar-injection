b40e52cb80d23ae605165f81bea80f55f0e60094
ZhengjunHUO <firelouiszj@hotmail.com>
2021-10-26 22:44:07 +0200

    adapt main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,10 @@
 package main
 
 import (
-	"log"
-	"os"
-	"os/signal"
-	"syscall"
-
 	"github.com/ZhengjunHUO/k8s-sidecar-injection/pkg/server"
 )
 
 func main() {
 	s := server.ServerInit()
-
-	go s.Start()
-
-	chInt := make(chan os.Signal, 1)
-	signal.Notify(chInt, os.Interrupt, syscall.SIGTERM)
-
-	<-chInt
-	log.Println("Receive interrupt signal, stop server ...")
-	s.Stop()
+	s.Run()
 }
