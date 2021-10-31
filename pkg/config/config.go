efd19759280b88543d57e386331856a7792b2fee
ZhengjunHUO <firelouiszj@hotmail.com>
2021-10-31 17:20:44 +0100

    fix config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -18,4 +18,5 @@ func init() {
 	flag.StringVar(&(Cfg.SidecarSpec), "spec", "./sidecarspec.yaml", "Path to sidecar specification")
 	flag.StringVar(&(Cfg.ServerCert), "cert", "./server.crt", "Path to server's certificate")
 	flag.StringVar(&(Cfg.ServerKey), "key", "./server.key", "Path to server's private key")
+	flag.Parse()
 }
